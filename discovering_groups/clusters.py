ba3931a9012ef96877852fe729b4407ab4d05c40
prakharchoudhary <prakhar2397@gmail.com>
2017-08-29 23:21:05 +0530

    add K-Means clustering

--- discovering_groups/clusters.py
+++ discovering_groups/clusters.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw
 
+
+############################################ HIERARCHICAL CLUSRTERING ######################################### 
 def readfile(filename):
   lines=[line for line in file(filename)]
   
@@ -111,73 +113,128 @@ def printclust(clust,labels=None,n=0):
   if clust.left!=None: printclust(clust.left,labels=labels,n=n+1)
   if clust.right!=None: printclust(clust.right,labels=labels,n=n+1)
 
-
-def getheight(clust):
-	# Is this an endpoint? Then the height is 1
-	if clust.left==None and clust.right==None: return 1
-
-	# otherwise the height is the same of the heights of
-	# each branch
-	return getheight(clust.left)+getheight(clust.right)
-
-def getdepth(clust):
-	# The distance of an endpont is 0.0
-	if clust.left==None and clust.right==None: return 0
-
-	# The distance of a branch is the greater of its two sides
-	# plus its own distance
-	return max(getdepth(clust.left), getdepth(clust.right))+clust.distance
-
-def drawdendogram(clust, labels, jpeg='clusters.jpg'):
-	# heigth and width
-	h = getheight(clust)*20
-	w = 1200
-	depth = getdepth(clust)
-
-	# width is fixed, so scale distances accordingly
-	scaling = float(w-150)/depth
-
-	# Create a new image with a white background
-	img = Image.new('RGB', (w,h), (255,255,255))
-	draw = ImageDraw.Draw(img)
-
-	draw.line((0,h/2,10,h/2),fill=(255,0,0))
-
-	# Draw the first node
-	drawnode(draw, clust, 10, (h/2), scaling, labels)
-	img.save(jpeg, 'JPEG')
-
-def drawnode(draw, clust, x, y, scaling, labels):
-	if clust.id<0:
-		h1 = getheight(clust.left)*20
-		h2 = getheight(clust.right)*20
-		top = y-(h1+h2)/2
-		bottom = y+(h1+h2)/2
-		#Line length
-		ll=clust.distance*scaling
-		# Vertical line from this cluster to children
-		draw.line((x, top+h1/2, x, bottom-h2/2), fill=(255,0,0))
-
-		# Horizontal line to left item
-		draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))
-		
-		# Horizontal line to right item
-		draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))
-
-		# Call the function to draw the left and right nodes
-		drawnode(draw,clust.left,x+ll,top+h1/2,scaling,labels)
-		drawnode(draw,clust.right,x+ll,bottom-h2/2,scaling,labels)
-
-	else:
-		# If this is an endpoint, draw the item label
-		draw.text((x+5,y-7), labels[clust.id], (0,0,0))
-
+################################# Rotate matrix to carry out COLUMN CLUSTERING #################
 def rotatematrix(data):
   newdata = []
   for i in range(len(data[0])):
     newrow = [data[j][i] for j in range(len(data))]
     newdata.append(newrow)
   return newdata
+################################################################################################
 
+
+
+############################## Create the dendogram ###############################
+
+def getheight(clust):
+  # Is this an endpoint? Then the height is 1
+  if clust.left==None and clust.right==None: return 1
+
+  # otherwise the height is the same of the heights of
+  # each branch
+  return getheight(clust.left)+getheight(clust.right)
+
+def getdepth(clust):
+  # The distance of an endpont is 0.0
+  if clust.left==None and clust.right==None: return 0
+
+  # The distance of a branch is the greater of its two sides
+  # plus its own distance
+  return max(getdepth(clust.left), getdepth(clust.right))+clust.distance
+
+def drawdendogram(clust, labels, jpeg='clusters.jpg'):
+  # heigth and width
+  h = getheight(clust)*20
+  w = 1200
+  depth = getdepth(clust)
+
+  # width is fixed, so scale distances accordingly
+  scaling = float(w-150)/depth
+
+  # Create a new image with a white background
+  img = Image.new('RGB', (w,h), (255,255,255))
+  draw = ImageDraw.Draw(img)
+
+  draw.line((0,h/2,10,h/2),fill=(255,0,0))
+
+  # Draw the first node
+  drawnode(draw, clust, 10, (h/2), scaling, labels)
+  img.save(jpeg, 'JPEG')
+
+def drawnode(draw, clust, x, y, scaling, labels):
+  if clust.id<0:
+    h1 = getheight(clust.left)*20
+    h2 = getheight(clust.right)*20
+    top = y-(h1+h2)/2
+    bottom = y+(h1+h2)/2
+    #Line length
+    ll=clust.distance*scaling
+    # Vertical line from this cluster to children
+    draw.line((x, top+h1/2, x, bottom-h2/2), fill=(255,0,0))
+
+    # Horizontal line to left item
+    draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))
+    
+    # Horizontal line to right item
+    draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))
+
+    # Call the function to draw the left and right nodes
+    drawnode(draw,clust.left,x+ll,top+h1/2,scaling,labels)
+    drawnode(draw,clust.right,x+ll,bottom-h2/2,scaling,labels)
+
+  else:
+    # If this is an endpoint, draw the item label
+    draw.text((x+5,y-7), labels[clust.id], (0,0,0))
+#############################################################################
+
+
+
+
+######################################################################################################				
+######################################################################################################        
+################################## K-MEANS CLUSTERING ################################################        
+######################################################################################################        
+######################################################################################################        
+import random
+
+def kcluster(rows, distance=pearson, k=4):
+  # Determine the min and max values for each point
+  ranges = [(min(row[i] for row in rows), max(row[i] for row in rows)) for i in range(len(rows[0]))]
+
+  # Create k randomly placed centroids
+  clusters = [[random.random()*(ranges[i][1] - ranges[i][0]) + ranges[i][0]
+              for i in range(len(rows[0]))] for j in range(k)]
+
+  lastmatches = None
+  for t in range(100):
+    print 'Iteration %d'% t
+    bestmatches = [[] for i in range(k)]
+
+
+    # Find which centroid is the closest for each row
+    for j in range(len(rows)):
+      row = rows[j]
+      bestmatch = 0
+      for i in range(k):
+        d = distance(clusters[i], row)
+        if d<distance(clusters[bestmatch], row):
+          bestmatch = i
+      bestmatches[bestmatch].append(j)
+
+    # If the results are the same as last time, this is complete
+    if bestmatches==lastmatches:
+      break
+    lastmatches=bestmatches
+
+    # Move the centroids to the average of their members
+    for i in range(k):
+      avgs = [0.0]*len(rows[0])
+      if len(bestmatches[i])>0:
+        for rowid in bestmatches[i]:
+          for m in range(len(rows[rowid])):
+            avgs[m]+=rows[rowid][m]
+        for j in range(len(avgs)):
+          avgs[j] /= len(bestmatches[i])
+        clusters[i]=avgs
   
-				
+  return bestmatches
